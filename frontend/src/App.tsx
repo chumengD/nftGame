38084990163e2d5180a6525f3ee8e5533cee52e7
chumengD <mildredcain9@gmail.com>
2025-11-11 19:40:13 +0800

    conncet wallet btn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+
 import { useState, useEffect } from 'react'
-import {createPublicClient, http } from 'viem'
+import {createPublicClient, createWalletClient, http, custom } from 'viem'
 import { mainnet } from 'viem/chains'
 import './App.css'
+
+interface Props{
+  connect: React.MouseEventHandler<HTMLButtonElement>;
+  fetchBalance: React.MouseEventHandler<HTMLButtonElement>;
+
+}
 
 const erc20Abi = [
   {
@@ -13,27 +20,58 @@ const erc20Abi = [
   }
 ];
 
-function App() {
-  const [blockNumber,setBlockNumber] = useState<bigint | null>(null);
-  
 
-  useEffect(()=>{
-    const client = createPublicClient({
+
+function Connectbtn({connect}:Props){
+  return <button onClick={connect}>Connncet Wallet</button>
+}
+
+function FetchBalancebtn({fetchBalance}:Props){
+  return <button onClick={fetchBalance}>查询余额</button>
+}
+
+const publicClient = createPublicClient({
       chain:mainnet,
       transport:http() 
-    })
+          })
 
-  client.getBlockNumber()
-    .then((num)=>{setBlockNumber(num)})
+function App() {
+  const [blockNumber,setBlockNumber] = useState<bigint | null>(null);
+  let signer;
   
+  useEffect(()=>{
+    publicClient.getBlockNumber()
+    .then((num)=>{setBlockNumber(num)}) 
 },[])
 
+    async function connectWallet(){
+      const [account] = await window.ethereum.request({
+        method:"eth_requestAccounts"
+    }) 
+     signer = createWalletClient({
+        account,
+        chain:mainnet,
+        transport:custom(window.ethereum)
+      })
+    }
+   
+    async function fetchBalance(){
+        const userAddress = "0xacA55C6b1bdDE9fAA5c4BAE786f491a1767D6708";
+        const result = await publicClient.getBalance({
+          address: userAddress
+      })
+      alert(`余额为：${result}`)
+
+
+    }
 
 
   return (
     <>
     <h1>viem demo</h1>
     <p>current block number:{blockNumber?.toString()}</p>
+    <Connectbtn connect={connectWallet}/>
+    <FetchBalancebtn fetchBalance={fetchBalance} />
 </>
   )
 }
